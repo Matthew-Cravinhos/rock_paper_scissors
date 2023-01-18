c4bec29de6b484d9754d646cf9cba2963c26a1d6
Matthew Cravinhos <matthewcravinhos@Matthews-MacBook-Pro.local>
2023-01-17 23:00:03 -0600

    Added winning criteria, reporting, and counters for rounds

--- rps.js
+++ rps.js
@@ -1,16 +1,42 @@
 // Declare const variables
 const choices = ["rock", "paper", "scissors"];
+let playerWins = 0;
+let computerWins = 0;
 
 // Core functionality
 const getComputerChoice = () => choices[Math.floor(Math.random() * choices.length)];
 const getPlayerChoice = () => prompt("Choose either rock, paper, or scissors!").toLowerCase();
+const addPlayerWin = () => {
+    playerWins++;
+    console.log("The player wins!")
+    return winsReport();
+}
+const addComputerWin = () => {
+    computerWins++;
+    console.log("The computer wins!");
+    return winsReport();
+}
+const winsReport = () => console.log("The player has " + playerWins + " wins and the computer has " + computerWins + " wins.");
+
 
 const gameRound = (playerChoice, computerChoice) => {
-    //let playerChoice = prompt("Choose either rock, paper, or scissors!").toLowerCase();
-
+    //check if valid option is entered first
     if (playerChoice === "rock" || playerChoice ==="paper" || playerChoice === "scissors") {
-        // add actual game checker here, followed by a counter for either the player or computer (whoever wins)
-        return console.log("checker working");
+        if (playerChoice === "rock" && computerChoice === "scissors") {
+            return addPlayerWin();
+        } else if (playerChoice === "paper" && computerChoice === "rock") {
+            return addPlayerWin();
+        } else if (playerChoice === "scissors" && computerChoice === "paper") {
+            return addPlayerWin();
+        } else if (computerChoice === "rock" && playerChoice === "scissors") {
+            return addComputerWin();
+        } else if (computerChoice === "paper" && playerChoice === "rock") {
+            return addComputerWin();
+        } else if (computerChoice === "scissors" && playerChoice === "paper") {
+            return addComputerWin();
+        } else if (playerChoice === "rock" && computerChoice === "rock" || playerChoice === "scissors" && computerChoice === "scissors" || playerChoice === "paper" && computerChoice === "paper") {
+            return console.log ("It's a tie! Please try again")
+        }
     } else {
         return console.log("No cheating silly :), please re-enter a valid option");
     }
